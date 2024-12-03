1a93f2ce7d50a7c2c6e11e2664bd0631afd84787
hcknbrgr <hcknbrgr@gmail.com>
2024-12-02 22:52:52 -0500

    create view for Todo

--- planner/views.py
+++ planner/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from .models import Todo
+from .serializers.todo import TodoSerializer
+
+
+class TodoView(viewsets.ModelViewSet):
+    serializer_class = TodoSerializer
+    queryset = Todo.objects.all()
