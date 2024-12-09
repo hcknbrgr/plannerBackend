0080c316ec4fbb5409792b72af2ef92547e12ef4
hcknbrgr <hcknbrgr@gmail.com>
2024-12-08 22:56:01 -0500

    Failing test for creating completed todo item

--- planner/models/todo/test_todo.py
+++ planner/models/todo/test_todo.py
@@ -12,3 +12,10 @@ def test_todo_creation_incomplete(todo_factory):
     assert td.title is not None
     assert td.description is not None
     assert not td.completed
+
+
+@pytest.mark.django_db
+def test_todo_creation_complete(todo_factory):
+    """Test creation of a todo list item, completed"""
+    td = todo_factory(completed=True)
+    assert not td.completed
