029ca77c1a890d542e75c699dbd3fb150ff275c2
hcknbrgr <hcknbrgr@gmail.com>
2024-12-08 22:54:37 -0500

    updated factory call to remove default value for completed

--- planner/models/todo/test_todo.py
+++ planner/models/todo/test_todo.py
@@ -8,7 +8,7 @@ import pytest
 @pytest.mark.django_db
 def test_todo_creation_incomplete(todo_factory):
     """Test creation of a todo list item, incomplete"""
-    td = todo_factory(completed=False)
+    td = todo_factory()
     assert td.title is not None
     assert td.description is not None
     assert not td.completed
