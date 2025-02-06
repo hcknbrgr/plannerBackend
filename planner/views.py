68951d093bb9fe9715bc1ea90794d0ab9bc1af45
hcknbrgr <hcknbrgr@gmail.com>
2025-02-06 17:01:43 -0500

    add subtaskView for the API endpoint to create new subtasks

--- planner/views.py
+++ planner/views.py
@@ -1,10 +1,17 @@
 # from django.shortcuts import render
 from rest_framework import viewsets
 
+from planner.models.subtask.subtask import Subtask
 from planner.models.todo.todo import Todo
+from planner.serializers.subtask.subtask import SubtaskSerializer
 from planner.serializers.todo.todo import TodoSerializer
 
 
 class TodoView(viewsets.ModelViewSet):
     serializer_class = TodoSerializer
     queryset = Todo.objects.all()
+
+
+class SubtaskView(viewsets.ModelViewSet):
+    serializer_class = SubtaskSerializer
+    queryset = Subtask.objects.all()
