cdc7dd6455465221f734a158dd8b32da6286faf3
hcknbrgr <hcknbrgr@gmail.com>
2025-02-26 21:43:33 -0500

    implement transform response testing

--- planner/utils/testing/test_openai_integration.py
+++ planner/utils/testing/test_openai_integration.py
@@ -1,12 +1,6 @@
-import json
+from planner.utils.openai_interface import transform_response
 
-import pytest
-
-
-@pytest.mark.django_db
-def test_openai_integration(subtask_factory):
-    # description = "testing in django"
-    response = """
+response = """
     {
         "task": "Break this task into subtasks",
         "subtasks": {
@@ -14,22 +8,33 @@ def test_openai_integration(subtask_factory):
             "2": "Divide the task into smaller logical steps"
         }
     }"""
-    parsed_response = json.loads(response)
-    task = parsed_response["task"]
-    subtasks = parsed_response["subtasks"]
-    step = next(iter(subtasks))
-    out1 = subtask_factory(step=step, description=subtasks[step])
-    step = list(subtasks.keys())[1]
-    out2 = subtask_factory(step=step, description=subtasks[step])
+
+
+def test_transformed_response():
+    task, subtask = transform_response(response)
 
     expected_out_task = "Break this task into subtasks"
-    expected_out_step_1 = "1"
-    expected_out_desc_1 = "Identify the main components of the task"
-    expected_out_step_2 = "2"
-    expected_out_desc_2 = "Divide the task into smaller logical steps"
+    expected_subtask = {
+        "1": "Identify the main components of the task",
+        "2": "Divide the task into smaller logical steps",
+    }
 
-    assert expected_out_task == task
-    assert expected_out_step_1 == out1.step
-    assert expected_out_desc_1 == out1.description
-    assert expected_out_step_2 == out2.step
-    assert expected_out_desc_2 == out2.description
+    assert task == expected_out_task
+    assert subtask == expected_subtask
+
+
+# @pytest.mark.django_db
+# def test_openai_integration(subtask_factory):
+#     # description = "testing in django"
+
+#     task, subtasks = transform_response(response)
+#     step = next(iter(subtasks))
+#     out1 = subtask_factory(step=step, description=subtasks[step])
+#     step = list(subtasks.keys())[1]
+#     out2 = subtask_factory(step=step, description=subtasks[step])
+
+#     assert expected_out_task == task
+#     assert expected_out_step_1 == out1.step
+#     assert expected_out_desc_1 == out1.description
+#     assert expected_out_step_2 == out2.step
+#     assert expected_out_desc_2 == out2.description
