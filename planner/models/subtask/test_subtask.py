7edd2340e8afd141b88bf7a8d727f2d26b48b239
hcknbrgr <hcknbrgr@gmail.com>
2025-02-26 21:18:32 -0500

    add subtask object creation test

--- planner/models/subtask/test_subtask.py
+++ planner/models/subtask/test_subtask.py
@@ -1,5 +1,7 @@
 import pytest
 
+from planner.models.subtask.subtask import Subtask
+
 
 @pytest.mark.django_db
 def test_subtask_creation(subtask_factory):
@@ -8,3 +10,13 @@ def test_subtask_creation(subtask_factory):
     assert subtask.step is not None
     assert subtask.description is not None
     assert subtask.todo is not None
+
+
+@pytest.mark.django_db
+def test_subtask_test_case(todo_factory):
+    td = todo_factory()
+    subtask = Subtask.objects.create(todo=td, step=1, description="desc")
+
+    assert subtask.todo == td
+    assert subtask.step == 1
+    assert subtask.description == "desc"
