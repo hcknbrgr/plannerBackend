3cf88b7c4cd3421ab74ad0e221c9e075a05b69f8
hcknbrgr <hcknbrgr@gmail.com>
2025-02-26 21:17:40 -0500

    implement test for todo object creation

--- planner/models/todo/test_todo.py
+++ planner/models/todo/test_todo.py
@@ -1,5 +1,7 @@
 import pytest
 
+from planner.models.todo.todo import Todo
+
 
 @pytest.mark.django_db
 def test_todo_creation_incomplete(todo_factory):
@@ -15,3 +17,12 @@ def test_todo_creation_complete(todo_factory):
     """Test creation of a todo list item, completed"""
     td = todo_factory(completed=True)
     assert td.completed
+
+
+@pytest.mark.django_db
+def test_todo_model():
+    obj = Todo.objects.create(title="test todo", description="learn TDD")
+
+    assert obj.title == "test todo"
+    assert obj.description == "learn TDD"
+    assert not obj.completed
