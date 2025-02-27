cfa21b8ef907881f0a21f1051de80765d7b00135
hcknbrgr <hcknbrgr@gmail.com>
2025-02-26 22:30:04 -0500

    test generate subtasks for a todo item

--- planner/utils/testing/test_openai_integration.py
+++ planner/utils/testing/test_openai_integration.py
@@ -1,6 +1,7 @@
 import pytest
 
 from planner.models.subtask.subtask import Subtask
+from planner.utils.openai_interface import generate_subtasks
 from planner.utils.openai_interface import transform_response
 
 response = """
@@ -31,24 +32,7 @@ def test_create_subtasks(todo_factory):
     _, subtasks = transform_response(response)
     td = todo_factory(description="Break this task into subtasks")
 
-    # generate_subtasks(td, subtasks)
+    generate_subtasks(td, subtasks)
 
     assert Subtask.objects.filter(todo=td).exists()
     assert Subtask.objects.filter(todo=td).count() == 2
-
-
-# @pytest.mark.django_db
-# def test_openai_integration(subtask_factory):
-#     # description = "testing in django"
-
-#     task, subtasks = transform_response(response)
-#     step = next(iter(subtasks))
-#     out1 = subtask_factory(step=step, description=subtasks[step])
-#     step = list(subtasks.keys())[1]
-#     out2 = subtask_factory(step=step, description=subtasks[step])
-
-#     assert expected_out_task == task
-#     assert expected_out_step_1 == out1.step
-#     assert expected_out_desc_1 == out1.description
-#     assert expected_out_step_2 == out2.step
-#     assert expected_out_desc_2 == out2.description
